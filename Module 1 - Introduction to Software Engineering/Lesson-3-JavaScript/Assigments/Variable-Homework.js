3a9f47adfb7537a47e7412894842c965e45b11fb
CristianBaraDev <cristianabv1993@gmail.com>
2023-12-12 16:08:34 -0500

    Homework 1

---
+++ Module 1 - Introduction to Software Engineering/Lesson-3-JavaScript/Assigments/Variable-Homework.js
@@ -0,0 +1,45 @@
+//SUBMIT A LINK TO THE GITHUB REPOSITORY CONTAINING THIS ASSIGNMENT
+// Create a .js file that satisfies assignment below 
+// Copy the final assignment text into this assignment to submit
+// For each exercise you'll want to log the result to the console to check your work
+
+
+//1. Create a set of variables that define an animal (age, height, name, etc...). Must be at least 6 variables.
+
+const animalName = "Eagle"
+const doesItFly = false
+const gender = "male"
+const weightInPounds = 15
+const doesItHasFeathers = true
+const diet = "Carnivore"
+
+
+
+const animal = 
+`${animalName},
+${doesItFly}. 
+${gender}, 
+${weightInPounds},
+${doesItHasFeathers}, 
+${diet}`
+
+console.log(animal)
+//2. Create a set of variables that define a vehicle. Must be at least 6 variables.
+const carMake = "BMW"
+const manual = false
+const color = "Gray"
+const year = 2023
+const convertible = false
+const topSpeed = "165 m/ph"
+
+
+
+const car = 
+`${carMake},
+${manual}. 
+${color}, 
+${year},
+${convertible}, 
+${topSpeed}`
+
+console.log(car)
