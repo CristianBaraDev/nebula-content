d933034a078faa768bf758202bdb77e690e98862
CristianBaraDev <cristianabv1993@gmail.com>
2024-01-02 23:22:16 -0500

    Homework button

---
+++ Module 1 - Introduction to Software Engineering/Lesson-5-DOM-Manipulation/Practice/DOM-Test/script.js
@@ -0,0 +1,4 @@
+const holdH1 = document.querySelector(".handing");
+console.log(holdH1);
+
+const allH1 = document.querySelector();
---
+++ Module 1 - Introduction to Software Engineering/Lesson-5-DOM-Manipulation/Practice/event-handling/script.js
@@ -0,0 +1,34 @@
+/** 
+// Grab the elements and log them to the console
+// Q1:  Attach this document to the index.html
+//Done
+
+// Q2:  Grab the second element that has the text Hello
+    const secondElement = document.getElementById("second");
+    console.log(secondElement);
+
+// Q3:  Grab the text in the h1 that says 'Good Afternoon'
+   const allElements = document.querySelectorAll('h1');
+   const goodAfterNoon = allElements[2];
+   console.log(goodAfterNoon);
+
+// Q4:  Grab the h1 with the text that says "Catch me if you can"
+    let catchMe = document.getElementsByClassName('catch-me')[0];
+    console.log(catchMe);
+
+// Q5:  Grab the span element and change the text to "Got ya"
+    const newSpan = document.querySelector('span');
+    newSpan.innerText ='Got ya';
+**/
+
+
+
+document.addEventListener("click",(e) =>{
+    const color = e.target.style.backgroundColor
+    if(color === "red"){
+        e.target.style.backgroundColor = "green"
+    }else{
+        e.target.style.backgroundColor = "red"
+    }
+
+})
